3cc18beaa733223d9752e38a572772675915a94e
eudes <eudes@users.noreply.github.com>
2020-07-16 19:04:00 +0000

    Deploy to GitHub pages

---
+++ _nuxt/lang-en-footer.3d8eb2d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{129:function(e,n,t){"use strict";t.r(n),n.default={credits:'a blog by <a target="_blank" href="https://eudes.es/" class="ani"><span>eudes ochoa</span></a>'}}}]);
---
+++ _nuxt/lang-en-index-page-head.f3de617.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{130:function(o,e,n){"use strict";n.r(e),e.default={title:"Eudes Ochoa",description:"a blog"}}}]);
